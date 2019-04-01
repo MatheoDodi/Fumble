7a911057ee0c0bf50f83d6011888d0fe788edfcf
Matthew Dodi <matthew.dodi@gmail.com>
2019-03-31 21:42:21 -0700

    type: fEat

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fumble.Data;
+using Fumble.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fumble.Controllers
@@ -10,18 +12,30 @@ namespace Fumble.Controllers
   [ApiController]
   public class ValuesController : ControllerBase
   {
+
+    private readonly DataContext _context;
+
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+    }
+
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public IActionResult GetValues()
     {
-      return new string[] { "value1", "value2", "value3" };
+      var values = _context.Values.ToList();
+
+      return Ok(values);
     }
 
     // GET api/values/5
-    [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    [HttpGet("{id:int}")]
+    public IActionResult GetValue(int id)
     {
-      return "value";
+      var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+      return Ok(value);
     }
 
     // POST api/values
