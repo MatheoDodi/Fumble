b064de2a99b9855d7b0676bd99202cb92923f527
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-05 23:04:12 -0700

    Type: feat - Implemented register method through authService

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -15,4 +15,8 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 }
