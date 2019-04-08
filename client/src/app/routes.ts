f824362c0141a83f8af1ff577ff1ffc2e46895e5
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-07 21:15:34 -0700

    type: feat - Added route guard to prevent users to acces certain routes without being logged in

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './_guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 import { MemberListComponent } from './member-list/member-list.component';
@@ -5,9 +6,16 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent }
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
