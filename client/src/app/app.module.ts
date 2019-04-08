5aebcc42f36c1fc90f1aad0f5b99049dd760a65e
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-07 21:01:03 -0700

    type: feat - Added angular router to handle multiple routes - Fixed styling in homepage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,17 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 // prettier-ignore
 @NgModule({
@@ -18,13 +23,17 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
@@ -32,7 +41,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       AlertifyService
    ],
    bootstrap: [
-      AppComponent,
+      AppComponent
    ]
 })
 export class AppModule {}
