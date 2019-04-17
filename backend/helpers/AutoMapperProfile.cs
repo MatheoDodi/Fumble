45507e93825a8541c046a15cd4e90c001bc40433
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-16 22:04:47 -0700

    Type: feat - Configured that mapper to show PhotoURL and Age of member

--- backend/helpers/AutoMapperProfile.cs
+++ backend/helpers/AutoMapperProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using backend.Dtos;
 using backend.Models;
@@ -8,8 +9,26 @@ namespace backend.helpers
   {
     public AutoMapperProfile()
     {
-      CreateMap<User, UserForListDto>();
-      CreateMap<User, UserForDetailedDto>();
+      CreateMap<User, UserForListDto>()
+        .ForMember(dest => dest.PhotoUrl, opt =>
+        {
+          opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.isMain).Url);
+        })
+        .ForMember(dest => dest.Age, opt =>
+        {
+          opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+        });
+
+      CreateMap<User, UserForDetailedDto>()
+        .ForMember(dest => dest.PhotoUrl, opt =>
+      {
+        opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.isMain).Url);
+      })
+      .ForMember(dest => dest.Age, opt =>
+      {
+        opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+      });
+
       CreateMap<Photo, PhotosForDetailedDto>();
     }
   }
