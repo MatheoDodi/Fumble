4999367bcced19d0a3d4f62d074f9d13482f29f0
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-17 21:55:59 -0700

    type: feat - Added ngx-gallery for user photos

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,11 @@ import { UserService } from './../../_services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivationEnd, ActivatedRoute } from '@angular/router';
+import {
+  NgxGalleryOptions,
+  NgxGalleryImage,
+  NgxGalleryAnimation
+} from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +16,8 @@ import { ActivationEnd, ActivatedRoute } from '@angular/router';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(
     private userService: UserService,
@@ -22,16 +29,32 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
   }
 
-  loadUser() {
-    this.userService.getUser(+this.route.snapshot.params['id']).subscribe(
-      (user: User) => {
-        this.user = user;
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
+  getImages() {
+    const imageUrls = this.user.photos.reduce((urls, photo) => {
+      urls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+
+      return urls;
+    }, []);
+
+    return imageUrls;
   }
 }
