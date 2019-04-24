a098d2d4f970e7c13d04cabda7de4055d0a1a06d
Matthew Dodi <matthew.dodi@gmail.com>
2019-04-23 22:42:00 -0700

    type: feat - Created new migration which includes the PhotoId prop

--- backend/Migrations/20190424054043_AddedPublicId.Designer.cs
+++ backend/Migrations/20190424054043_AddedPublicId.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Fumble.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fumble.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190424054043_AddedPublicId")]
+    partial class AddedPublicId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
